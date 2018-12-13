2e01a13149e2132df90c6f50ba671e4a5b726b9d
AAkriti17 <akritikapur8@gmail.com>
2018-12-14 01:10:03 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.codebind;
+
+public class App {
+
+	public String Sample()
+	{
+		return "sample";
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
