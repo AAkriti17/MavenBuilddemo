ba4190439913a438d728dbcaceccbac041aba6fe
AAkriti17 <akritikapur8@gmail.com>
2018-12-15 13:39:55 +0530

    Update App.java

--- App.java
+++ App.java
@@ -9,6 +9,11 @@ public class App {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i=4;
+		int b=4;
+		int c=0;
+		c=i+b;
+		System.out.println(c);
 
 	}
 
